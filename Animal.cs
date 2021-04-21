a69fb3f1ea0239a461c455edd7ccec4468e74597
Myrellags <myrellags@gmail.com>
2021-04-21 15:00:07 -0300

    1

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityVet
+{
+    class Animal
+    {
+        public int AnimalID { get; set; }
+        public int PessoaID { get; set; }
+        public string NomePet { get; set; }
+        public string TipoPet { get; set; }
+    }
+}
